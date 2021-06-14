b814ce20242b8ecd378dd722e437261cce4b2bd2
Priyanka <priyankabadhani1@gmail.com>
2021-06-14 17:03:55 +0530

    Third commit

--- backend/data/products.js
+++ backend/data/products.js
@@ -35,4 +35,4 @@ const products =[
    
 
 ]
-export default products
+module.exports= products
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import{Row, Col}from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
+
 const HomeScreen = () => {
+    const [products,setProducts]= useState([])
+
+    useEffect(() => {
+        const fetchproducts = async() =>{
+            const {data}= await axios.get('/api/products')
+
+         setProducts(data)   
+        }
+        fetchproducts()
+    },[])
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
                {products.map(product =>(
-                   <Col sm={'auto'} md ={'auto'} lg={'auto'} xs={'auto'}>
+                   <Col key ={product._id} sm={12} md ={6} lg={4} xs={3}>
                     <Product product={product}/>
                    </Col>
                ))}
